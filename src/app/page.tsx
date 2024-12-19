947ef3c55064faecba62282b70bc8f74a00f1327
Ankur Jain <49987711+rezilience@users.noreply.github.com>
2024-12-19 16:25:05 -0500

    humble beginnings

---
+++ src/app/page.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import React from 'react';
+
+import { Heading, Text, Flex, Button, Grid, Icon, InlineCode, Logo, LetterFx, Arrow } from '@/once-ui/components';
+import Link from 'next/link';
+
+export default function Home() {
+  const links = [
+    {
+      href: "https://once-ui.com/docs/theming",
+      title: "Vocal Vibes",
+      description: "The Singer in Me",
+    },
+    {
+      href: "https://once-ui.com/docs/flexComponent",
+      title: "Tech Tracks",
+      description: "The Techie in Me",
+    },
+    {
+      href: "https://once-ui.com/docs/typography",
+      title: "Money Maven",
+      description: "The Finance Fanatic in Me",
+    },
+    {
+      href: "https://once-ui.com/docs/typography",
+      title: "Fit Frenzy",
+      description: "The Fitness Buff in Me",
+    },
+  ];
+
+  return (
+    <Flex
+      fillWidth paddingTop="l" paddingX="l"
+      direction="column" alignItems="center" flex={1}>
+      <Flex
+        position="relative"
+        as="section" overflow="hidden"
+        fillWidth minHeight="0" maxWidth={68}
+        direction="column" alignItems="center" flex={1}>
+        <Flex
+          as="main"
+          direction="column" justifyContent="center"
+          fillWidth fillHeight padding="l" gap="l">
+          <Flex
+            mobileDirection="column"
+            fillWidth gap="24">
+            <Flex
+              position="relative"
+              flex={2} paddingTop="56" paddingX="xl">
+              <Logo size="xl" icon={false} style={{ zIndex: '1' }} href="https://once-ui.com" />
+            </Flex>
+            <Flex
+              position="relative"
+              flex={4} gap="24" marginBottom="104"
+              direction="column">
+              <InlineCode
+                className="shadow-m"
+                style={{
+                  width: 'fit-content',
+                  padding: 'var(--static-space-8) var(--static-space-16)',
+                  backdropFilter: 'blur(var(--static-space-1))'
+                }}>
+                Start by editing <span className="brand-on-background-medium">app/page.tsx</span>
+              </InlineCode>
+              <Heading
+                wrap="balance"
+                variant="display-strong-s">
+                <span className="font-code">
+                  <LetterFx
+                    trigger="instant">
+                    Welcome to My World! Step inside and explore the passions that fuel my geeky pursuits.
+                  </LetterFx>
+                </span>
+              </Heading>
+              {/* <Button
+                id="readDocs"
+                href="https://once-ui.com/docs"
+                variant="secondary">
+                <Flex alignItems="center">
+                  Read docs
+                  <Arrow trigger="#readDocs" />
+                </Flex>
+              </Button> */}
+            </Flex>
+          </Flex>
+          <Grid
+            radius="l"
+            border="neutral-medium"
+            borderStyle="solid-1"
+            columns="repeat(4, 1fr)"
+            tabletColumns="2col"
+            mobileColumns="1col"
+            gap="m"
+            fillWidth>
+            {links.map((link) => (
+              <Link
+                target="_blank"
+                style={{ padding: 'var(--responsive-space-m)' }}
+                key={link.href}
+                href={link.href}>
+                <Flex
+                  fillWidth paddingY="8" gap="8"
+                  direction="column">
+                  <Flex
+                    fillWidth gap="8"
+                    alignItems="center">
+                    <Text
+                      variant="body-strong-m" onBackground="neutral-strong">
+                      {link.title}
+                    </Text>
+                    <Icon size="s" name="arrowUpRight" />
+                  </Flex>
+                  <Text
+                    variant="body-default-s" onBackground="neutral-weak">
+                    {link.description}
+                  </Text>
+                </Flex>
+              </Link>
+            ))}
+          </Grid>
+        </Flex>
+      </Flex>
+      <Flex
+        as="footer"
+        position="relative"
+        fillWidth paddingX="l" paddingY="m"
+        justifyContent="space-between">
+        <Text
+          variant="body-default-s" onBackground="neutral-weak">
+          2024 Once UI, <Link href="https://github.com/once-ui-system/nextjs-starter?tab=MIT-1-ov-file">MIT License</Link>
+        </Text>
+        <Flex
+          gap="12">
+          <Button
+            href="https://github.com/once-ui-system/nextjs-starter"
+            prefixIcon="github" size="s" variant="tertiary">
+            GitHub
+          </Button>
+          <Button
+            href="https://discord.com/invite/5EyAQ4eNdS"
+            prefixIcon="discord" size="s" variant="tertiary">
+            Discord
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+}
